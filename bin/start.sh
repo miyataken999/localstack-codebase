fb5dc161a07c7524581442750ae76b5172d584c7
kenichi miyata <miyataken999@bpmboxes.com>
2024-02-17 06:57:33 +0000

    sds

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+export EXTRA_CORS_ALLOWED_ORIGINS '*'
+export DISABLE_CORS_CHECKS 1
+export DISABLE_CUSTOM_CORS_APIGATEWAY 1
 # start LocalStack container in the background
 echo "Starting LocalStack instance ..."
 DEBUG=1 localstack start -d
